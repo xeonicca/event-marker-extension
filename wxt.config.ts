9dcdf606c7d5f28c0b23fa8cf8f66e984ac51ed7
Steve Yu <steve.yu@amazingtalker.com>
2024-08-26 18:35:07 +0800

    feat: change extension icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
+    "name": "AT Event Marker",
     "content_security_policy": {
       "sandbox": "sandbox allow-scripts; script-src 'self' https://www.gstatic.com/ https://*.firebaseio.com https://www.googleapis.com"
     },
